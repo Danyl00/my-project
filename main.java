af8ff532f76ba46043f0eea81fcc8eda858636fd
Danyl <danyl.nyzhnyk2010@gmail.com>
2024-01-10 18:51:11 +0200

    new array

--- main.java
+++ main.java
@@ -1,7 +1,28 @@
-public class main {
-    public static void main(String[] args) {
+import java.util.Random;
 
-        System.out.println("hello world");
+public class main {
+    public static void main(String[] args){
+        Random random=new Random();
+        int size=10;
+        int max;
+        int[]d=new int[size];
+        for (int i = 0; i <size; i++) {
+            d[i]=random.nextInt(1,100);
+
+        }
+        for (int i = 0; i <size ; i++) {
+            System.out.print(d[i]+" ");
+        }
+        max=d[0];
+        for (int i = 0; i <size ; i++) {
+            if(max<d[i]){
+               max=d[i];
+
+            }
+        }
+        System.out.println( );
+        System.out.println(max);
+
     }
 
 }
