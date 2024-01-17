b942d17a2d2575cbc9114fc8cfd0f96c80e21daf
Danyl <danyl.nyzhnyk2010@gmail.com>
2024-01-17 18:06:19 +0200

    new Code

--- main.java
+++ main.java
@@ -1,8 +1,11 @@
 import java.util.Random;
 
 public class main {
-    public static void main(String[] args){
-        Random random=new Random();
+    public static void main(String[] args) {
+
+
+
+      /*
         int size=10;
         int max;
         int[]d=new int[size];
@@ -21,8 +24,75 @@ public class main {
             }
         }
         System.out.println( );
-        System.out.println(max);
+        System.out.println(ma*/
+
+
+   /* Random random = new Random();
+    int size = 10;
+
+
+    int[] g = new int[size];
+    int min=0;
+    int max=0;
+  for(int i = 0;i<size;i++){
+
+        g[i] = random.nextInt(-1, 10);
 
     }
+        for (int i = 0; i <size ; i++) {
+            System.out.print(g[i]+" ");
+
+        }
+        for (int i = 0; i <size ; i++) {
+            if(g[i]==0){
+                g[i]=4;
+            }
+
+        }
+        System.out.println();
+        for (int i = 0; i <size ; i++) {
+            System.out.print(g[i]+" ");
+
+        }
+        for (int i = 0; i <size ; i++) {
+            if(min>g[i]){
+                min=g[i];
+            }
+            if(max<g[i]){
+                max=g[i];
+            }
+        }
+        System.out.println();
+        System.out.println(min+" "+max);*/
+
+
+
+
+
+        /*int[]g={1,-5,4,2,6,3,-1,4,-7,-3};
+        for (int i = 0; i < 9; i++) {
+            System.out.print(g[i]);
+        }
+        System.out.println();
+        for (int i = 9; i != 0 ; i--) {
+            if (g[i]>0){
+                System.out.println(g[i]+" ");
+                break;
+            }
+
+        }
+
+        System.out.println();
+        for (int i = 0; i <9 ; i++) {
+            if(g[i]<0){
+                System.out.println(g[i]+" ");
+                break;
+            }
+        }*/
+
+
 
 }
+
+}
+
